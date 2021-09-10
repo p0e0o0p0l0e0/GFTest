f05ded2224eb587ee29f1823bc1c7a4b8770b979
CYOU-INC\zhanglinqi <p0e0o0p0l0e0@sina.com>
2021-09-10 16:24:47 +0800

    (ensureSize - 1 + BlockSize) / BlockSize * BlockSize; 这种写法为了4KB对齐（BlockSize为1024 * 4）

---
+++ BlockSizeAlign/BlockSizeAlign/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BlockSizeAlign
+{
+    class Program
+    {
+        private const int BlockSize = 1024 * 4;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] testInt = { 0, 1, 2, 1024 * 4, 1024 * 4 + 1, 1024 * 1024 * 4 };
+            for(int i = 0; i < testInt.Length; i++)
+            {
+                int size = (testInt[i] - 1 + BlockSize) / BlockSize * BlockSize;
+                Console.WriteLine(testInt[i] + " size: " + size);
+            }       
+        }
+    }
+}
+
+//Hello World!
+//0 size: 0
+//1 size: 4096
+//2 size: 4096
+//4096 size: 4096
+//4097 size: 8192
+//4194304 size: 4194304
