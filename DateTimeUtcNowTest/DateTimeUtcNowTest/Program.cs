6d8948b2a6fbe095ba26d83827477b576ab7a83d
CYOU-INC\zhanglinqi <p0e0o0p0l0e0@sina.com>
2021-09-10 15:45:17 +0800

    DateTime.Now和UtcNow比较，由于UtcNow没有获取区域信息，因此性能更高

---
+++ DateTimeUtcNowTest/DateTimeUtcNowTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DateTimeUtcNowTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            DateTime dt;
+
+            DateTime time = DateTime.Now;
+            for(int i = 0; i < 10000; i++)
+            {
+                dt = DateTime.Now.AddSeconds(1);
+            }
+            Console.WriteLine("Now : {0}", (DateTime.Now - time).TotalMilliseconds); // 80.1674
+
+            time = DateTime.UtcNow;
+            TimeZoneInfo ChTimeZone = TimeZoneInfo.FindSystemTimeZoneById("China Standard Time");
+            for (int i = 0; i < 10000; i++)
+            {
+                dt = DateTime.UtcNow.AddSeconds(1);
+                ChTimeZone.GetUtcOffset(dt);
+            }
+            Console.WriteLine("UtcNow : {0}", (DateTime.UtcNow - time).TotalMilliseconds); // 64.1545
+
+        }
+    }
+}
