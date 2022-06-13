4a55d7f1764cd8742088c677434045d69cc55192
CYOU-INC\zhanglinqi <p0e0o0p0l0e0@sina.com>
2022-06-13 21:59:05 +0800

    leetcode: 双指针，344 双指针翻转字符串

---
+++ LeetCode/LeetCode/Algorithm/DoublePointer/344.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace _344
+{//双指针：
+    public class Solution
+    {
+        // 240ms 66.3MB
+        public void ReverseString(char[] s)
+        {
+            char temp;
+            for(int i = 0, j =  s.Length - 1; i < j;i++,j--)
+            {
+                temp = s[i];
+                s[i] = s[j];
+                s[j] = temp;
+            }
+        }
+    }
+}
