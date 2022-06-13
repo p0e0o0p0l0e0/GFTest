fdf0762a3f86ec099c18319cf6873413f5f88326
CYOU-INC\zhanglinqi <p0e0o0p0l0e0@sina.com>
2022-06-13 20:37:20 +0800

    leetcode: 双指针，283 把所有0移动到数组最后，其他数值顺序不变。

---
+++ LeetCode/LeetCode/Algorithm/DoublePointer/283.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace _283
+{//双指针：
+    public class Solution
+    {
+        // 136ms 52.7MB
+        // 双指针，i记录非零位置，j向后移动寻找非零值
+        public void MoveZeroes(int[] nums)
+        {
+            for (int i = 0, j = 0; j < nums.Length; j++)
+            {
+                if(nums[j] != 0)
+                {
+                    if(i != j)
+                    {
+                        nums[i] = nums[j];
+                        nums[j] = 0;
+                    }
+                    i++;
+                }
+            }
+        }
+    }
+}
--- LeetCode/LeetCode/Program.cs
+++ LeetCode/LeetCode/Program.cs
@@ -5,9 +5,10 @@ internal class Program
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello World!");
-        _189.Solution sol = new _189.Solution();
-        int[] nums = new int[] {-1};
-        int[] ret = sol.Rotate1(nums, 2);
+        _283.Solution sol = new _283.Solution();
+        int[] nums = new int[] {2};
+        sol.MoveZeroes(nums);
+        int[] ret = nums;
         for(int i = 0; i < ret.Length; i++)
         {
             Console.Write(ret[i]);
