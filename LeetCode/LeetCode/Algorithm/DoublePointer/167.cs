fa85e403ca65b9e7274a491ba3858960c3beeda7
CYOU-INC\zhanglinqi <p0e0o0p0l0e0@sina.com>
2022-06-13 21:14:03 +0800

    leetcode: 双指针，167 非递减序列数组，寻找双个数之和等于目标值。

---
+++ LeetCode/LeetCode/Algorithm/DoublePointer/167.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+
+namespace _167
+{//双指针：
+    public class Solution
+    {
+        // 152ms 45.4MB
+        // 未找到匹配值的数字存在Dic中，用于后续数字的对应值在dic中匹配。时间复杂度O(n)
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            int temp = 0;
+            for(int i = 0; i < numbers.Length;i++)
+            {
+                temp = target - numbers[i];
+                if(!dic.ContainsKey(temp))
+                {
+                    dic[numbers[i]] = i;
+                }
+                else
+                {
+                    return new int[2] { dic[temp] + 1, i + 1 };
+                }
+            }
+            return null;
+        }
+
+        // 480ms 45.1MB
+        // 因为序列递增，依次遍历数组的每一个值，去后续序列里寻找匹配值。时间复杂度O(nlogn)
+        public int[] TwoSum1(int[] numbers, int target)
+        {
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                for(int j = i+1; j < numbers.Length; j++)
+                {
+                    int temp = numbers[i] + numbers[j];
+                    if (temp > target)
+                        break;
+                    if (temp == target)
+                    {
+                        return new int[2] { i + 1, j + 1 };
+                    }
+                }
+            }
+            return null;
+        }
+
+        // 128ms 45.3MB
+        // 前后两个指针，当和大于target，则后指针往前移，如果小于target则前指针向后移
+        public int[] TwoSums2(int[] numbers, int target)
+        {
+            for (int i = 0, j = numbers.Length - 1; ;)
+            {
+                if (i >= j)
+                    break;
+                int temp = numbers[i] + numbers[j];
+                if (temp == target)
+                    return new int[2] { i + 1, j + 1 };
+                else if (temp > target)
+                    j--;
+                else
+                    i++;
+            }
+            return null;
+        }
+    }
+}
--- LeetCode/LeetCode/Program.cs
+++ LeetCode/LeetCode/Program.cs
@@ -5,10 +5,10 @@ internal class Program
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello World!");
-        _283.Solution sol = new _283.Solution();
+        _167.Solution sol = new _167.Solution();
         int[] nums = new int[] {2};
-        sol.MoveZeroes(nums);
-        int[] ret = nums;
+        int[] ret = sol.TwoSums2(nums, 4);
+        //int[] ret = nums;
         for(int i = 0; i < ret.Length; i++)
         {
             Console.Write(ret[i]);
