b9797b1662abdb6674bc16cd8891fbcc07a607ba
CYOU-INC\zhanglinqi <p0e0o0p0l0e0@sina.com>
2022-06-14 16:37:40 +0800

    leetcode: 双指针，577 双指针记录每个需要翻转的单词进行string reverse 76ms 43.6MB

---
+++ LeetCode/LeetCode/Algorithm/DoublePointer/557.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace _557
+{//双指针：
+    public class Solution
+    {
+        // 76ms 43.6MB
+        // 转char[]，每遇到空格就把前面单词反转。
+        public string ReverseWords(string s)
+        {
+            char[] c = s.ToCharArray();
+            int from = -1, to = -1;
+            for(int i = 0; i < c.Length; i++)
+            {
+                if (from == -1 && c[i] != ' ')
+                {
+                    from = i;
+                }
+                else if(c[i] == ' ')
+                {
+                    to = i - 1;
+                    ReverseString(c, from, to);
+                    from = -1;
+                }
+                else if(i == c.Length - 1)
+                {
+                    to = i;
+                    ReverseString(c, from, to);
+                    from = -1;
+                }
+            }
+            return new string(c);
+        }
+        private void ReverseString(char[] s, int from, int to)
+        {
+            char temp;
+            for (int i = from, j = to; i < j; i++, j--)
+            {
+                temp = s[i];
+                s[i] = s[j];
+                s[j] = temp;
+            }
+        }
+    }
+}
--- LeetCode/LeetCode/Program.cs
+++ LeetCode/LeetCode/Program.cs
@@ -5,14 +5,15 @@ internal class Program
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello World!");
-        _167.Solution sol = new _167.Solution();
+        _557.Solution sol = new _557.Solution();
         int[] nums = new int[] {2};
-        int[] ret = sol.TwoSums2(nums, 4);
+        string s = sol.ReverseWords("contest");
+        Console.WriteLine(s);
         //int[] ret = nums;
-        for(int i = 0; i < ret.Length; i++)
-        {
-            Console.Write(ret[i]);
-            Console.Write("    ");
-        }
+        //for(int i = 0; i < ret.Length; i++)
+        //{
+        //    Console.Write(ret[i]);
+        //    Console.Write("    ");
+        //}
     }
 }
