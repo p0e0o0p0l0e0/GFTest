133ad4ab55a2ccc7271279290e8e68276c8cfdd1
CYOU-INC\zhanglinqi <p0e0o0p0l0e0@sina.com>
2022-06-15 15:27:03 +0800

    leetcode: 双指针，19. 删除链表的倒数第 N 个结点 方法1：快慢指针：fast先向后移动，当slow和fast间距等于n时同时向后移动。时间复杂度O(n)，空间复杂度O(1) 。 80ms 38.2MB 方法2：递归链表，可以倒叙计算cur，与n相等时返回head.next。 84ms, 37.8MB

---
+++ LeetCode/LeetCode/Algorithm/DoublePointer/19.cs
@@ -0,0 +1,64 @@
+﻿namespace _19 //19. 删除链表的倒数第 N 个结点
+{//双指针：
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        // 80ms 38.2MB
+        // 快慢指针：fast先向后移动，当slow和fast间距等于n时同时向后移动。时间复杂度O(n)，空间复杂度O(1)
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            ListNode slow = null, fast = head;
+            int count = 0;
+            while (fast != null)
+            {
+                if (count >= n)
+                {
+                    if (slow == null)
+                    {
+                        slow = head;
+                    }
+                    else
+                    {
+                        slow = slow.next;
+                    }
+                }
+                count++;
+                fast = fast.next;
+            }
+            if (slow != null && slow.next != null)
+            {
+                slow.next = slow.next.next;
+            }
+            else if(slow == null && n == count)
+            {// 如果slow是空说明n>=count，相等则说明刚好要删除第一个节点
+                head = head.next;
+            }
+            return head;
+        }
+
+        // 84ms, 37.8MB
+        // 递归链表，可以倒叙计算cur，与n相等时返回head.next
+        int cur = 0;
+        public ListNode RemoveNthFromEnd1(ListNode head, int n)
+        {
+            if (head == null)
+                return null;
+            head.next = RemoveNthFromEnd1(head.next, n);
+            cur++;
+            if (cur == n)
+                return head.next;
+            return head;
+        }
+    }
+}
--- LeetCode/LeetCode/Program.cs
+++ LeetCode/LeetCode/Program.cs
@@ -1,4 +1,4 @@
-﻿using _876;
+﻿using _19;
 using System;
 
 internal class Program
@@ -6,9 +6,9 @@ internal class Program
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello World!");
-        _876.Solution sol = new _876.Solution();
+        _19.Solution sol = new _19.Solution();
         ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6))))));
-        ListNode middle = sol.MiddleNode1(head);
+        ListNode middle = sol.RemoveNthFromEnd1(head, 1);
         while (middle != null)
         {
             Console.Write(middle.val);
