8188118828f1f5dd18a7addae14dd94f676789b6
CYOU-INC\zhanglinqi <p0e0o0p0l0e0@sina.com>
2022-06-15 10:55:29 +0800

    leetcode: 双指针，876 快慢指针：慢指针每次移动一位，快指针每次移动两位。时间复杂度O(n)，空间复杂度O(1) 76ms 37.1MB

---
+++ LeetCode/LeetCode/Algorithm/DoublePointer/876.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _876
+{//双指针：
+    public class ListNode {
+        public int val;
+        public ListNode next;
+        public ListNode(int val=0, ListNode next=null)
+        {
+          this.val = val;
+          this.next = next;
+        }
+  }
+ 
+    public class Solution
+    {
+        // 76ms 37.1MB
+        // 快慢指针：count记录向后移的个数，偶数时centerPointer向后移动一位。时间复杂度O(n)，空间复杂度O(1)
+        public ListNode MiddleNode(ListNode head)
+        {
+            int count = 0;
+            ListNode slow = head, fast = head;
+            while(fast.next != null)
+            {
+                count++;
+                fast = fast.next;
+                if((count + 1) % 2 == 0)
+                {
+                    slow = slow.next;
+                }
+            }
+            return slow;
+        }
+
+        // 思路与时长同上，写法更简洁
+        public ListNode MiddleNode1(ListNode head)
+        {
+            ListNode slow = head, fast = head;
+            while (fast != null && fast.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            return slow;
+        }
+    }
+}
--- LeetCode/LeetCode/Program.cs
+++ LeetCode/LeetCode/Program.cs
@@ -1,19 +1,19 @@
-﻿using System;
+﻿using _876;
+using System;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello World!");
-        _557.Solution sol = new _557.Solution();
-        int[] nums = new int[] {2};
-        string s = sol.ReverseWords("contest");
-        Console.WriteLine(s);
-        //int[] ret = nums;
-        //for(int i = 0; i < ret.Length; i++)
-        //{
-        //    Console.Write(ret[i]);
-        //    Console.Write("    ");
-        //}
+        _876.Solution sol = new _876.Solution();
+        ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6))))));
+        ListNode middle = sol.MiddleNode1(head);
+        while(middle != null)
+        {
+            Console.Write(middle.val);
+            Console.Write("    ");
+            middle = middle.next;
+        }
     }
 }
