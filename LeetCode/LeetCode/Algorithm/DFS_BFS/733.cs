842a3859966c41b458e4e0ff2fd5191f8bb51862
CYOU-INC\zhanglinqi <p0e0o0p0l0e0@sina.com>
2022-06-21 14:47:21 +0800

    leetcode: 滑动窗口，733. 图像渲染 DFS深度搜索

---
+++ LeetCode/LeetCode/Algorithm/DFS_BFS/733.cs
@@ -0,0 +1,32 @@
+﻿namespace _733// 733. 图像渲染
+{// 深度优先DFS 广度优先BFS：
+
+
+    // DFS 132ms 45MB
+    public class Solution
+    {
+        public int[][] FloodFill(int[][] image, int sr, int sc, int newColor)
+        {
+            DFS(image, sr, sc, image[sr][sc], newColor);
+            return image;
+        }
+
+        private void DFS(int[][] image, int x, int y, int oldColor, int newColor)
+        {
+            if (x < 0 || y < 0 || x >= image.Length || y >= image[0].Length)
+            {
+                return;
+            }
+            if (image[x][y] != oldColor || image[x][y] == newColor)
+            {
+                return;
+            }
+
+            image[x][y] = newColor;
+            DFS(image, x + 1, y, oldColor, newColor);
+            DFS(image, x - 1, y, oldColor, newColor);
+            DFS(image, x, y + 1, oldColor, newColor);
+            DFS(image, x, y - 1, oldColor, newColor);
+        }
+    }
+}
