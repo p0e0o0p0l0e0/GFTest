71a2f121c5004482787af3196563615650b0178d
CYOU-INC\zhanglinqi <p0e0o0p0l0e0@sina.com>
2022-06-28 11:53:55 +0800

    leetcode: 滑动窗口，116. 填充每个节点的下一个右侧节点指针 BFS和DFS

---
+++ LeetCode/LeetCode/Algorithm/BTree/116.cs
@@ -0,0 +1,83 @@
+﻿namespace _116 // 116. 填充每个节点的下一个右侧节点指针
+{// 二叉树
+
+    using System;
+    using System.Collections.Generic;
+
+    public class Node
+    {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() { }
+
+        public Node(int _val)
+        {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next)
+        {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+
+    public class Solution
+    {
+        // BFS 92ms 42.7MB
+        public Node Connect(Node root)
+        {
+            if (root != null)
+            {
+                int i = 0, count = 0;
+                Queue<Node> queue = new Queue<Node>();
+                queue.Enqueue(root);
+                while(queue.Count > 0)
+                {
+                    Node temp = queue.Dequeue();
+                    count++;
+                    if(count == Math.Pow(2, i))
+                    {
+                        temp.next = null;
+                        count = 0;
+                        i++;
+                    }
+                    else
+                    {
+                        temp.next = queue.Peek();
+                    }
+                    if(temp.left != null)
+                    {
+                        queue.Enqueue(temp.left);
+                        queue.Enqueue(temp.right);
+                    }
+                }
+            }
+            return root;
+        }
+
+        // DFS 88ms 42.3MB
+        public Node Connect1(Node root)
+        {
+            if(root != null)
+            {
+                if(root.left != null)
+                {
+                    root.left.next = root.right;
+                    if(root.next != null)
+                    {
+                        root.right.next = root.next.left;
+                    }
+                    root.left = Connect1(root.left);
+                    root.right = Connect1(root.right);
+                }
+            }
+            return root;
+        }
+    }
+}
--- LeetCode/LeetCode/Program.cs
+++ LeetCode/LeetCode/Program.cs
@@ -1,4 +1,4 @@
-﻿using _617;
+﻿using _116;
 using System;
 
 internal class Program
@@ -6,23 +6,12 @@ internal class Program
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello World!");
-        Solution sol = new(); 
-        TreeNode root1 = new TreeNode(1);
-        root1.left = new TreeNode(3);
-        root1.right = new TreeNode(2);
-        root1.left.left = new TreeNode(5);
-
-        TreeNode root2 = new TreeNode(2);
-        root2.left = new TreeNode(1);
-        root2.right = new TreeNode(3);
-        root2.left.right = new TreeNode(4);
-        root2.right.right = new TreeNode(7);
-
-        TreeNode maxarea = sol.MergeTrees(root1, root2);
-        treeDfs(maxarea);
+        Solution sol = new();
+        Node root = new Node(1, new Node(2, new Node(4), new Node(5), null), new Node(3, new Node(6), new Node(7), null), null);
+        sol.Connect1(root);
     }
 
-    public static void treeDfs(TreeNode root)
+    public static void treeDfs(Node root)
     {
         if (root == null)
         {
