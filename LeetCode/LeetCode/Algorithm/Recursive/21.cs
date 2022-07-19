431323ce36ce2538719db049f07962d0232f54ff
CYOU-INC\zhanglinqi <p0e0o0p0l0e0@sina.com>
2022-07-19 18:00:24 +0800

    leetcode: 21. 合并两个有序链表 新建一个链表，递归和迭代两个方法

--- LeetCode/LeetCode/Algorithm/Recursive/21.cs
+++ LeetCode/LeetCode/Algorithm/Recursive/21.cs
@@ -14,7 +14,54 @@
 
     public class Solution
     {
+        // 迭代
         public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            ListNode prehead = new ListNode(-1);
+            ListNode temp = prehead;
+
+            while(list1 != null && list2 != null)
+            {
+                if(list1.val < list2.val)
+                {
+                    temp.next = list1;
+                    list1 = list1.next;
+                }
+                else
+                {
+                    temp.next = list2;
+                    list2 = list2.next;
+                }
+                temp = temp.next;
+            }
+            temp.next = list1 != null ? list1 : list2;
+            return prehead.next;
+        }
+
+        // 递归 92ms 39.1MB 空间为m+n，因为递归占用栈空间，时间m+n
+        public ListNode MergeTwoLists2(ListNode list1, ListNode list2)
+        {
+            if (list1 == null)
+            {
+                return list2;
+            }
+            else if (list2 == null)
+            {
+                return list1;
+            }
+            else if (list1.val < list2.val)
+            {
+                list1.next = MergeTwoLists2(list1.next, list2);
+                return list1;
+            }
+            else
+            {
+                list2.next = MergeTwoLists2(list1, list2.next);
+                return list2;
+            }
+        }
+
+        public ListNode MergeTwoLists1(ListNode list1, ListNode list2)
         {
             ListNode list = new ListNode(0);
             Recursive(list, list1, list2);
