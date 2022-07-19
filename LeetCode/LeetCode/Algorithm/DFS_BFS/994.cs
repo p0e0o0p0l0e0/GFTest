3789cbaccb4aecea9067fa8b5ee6cd2ef9d88647
CYOU-INC\zhanglinqi <p0e0o0p0l0e0@sina.com>
2022-07-19 15:59:39 +0800

    leetcode: 994. 腐烂的橘子 多源广度优先 代码整理

--- LeetCode/LeetCode/Algorithm/DFS_BFS/994.cs
+++ LeetCode/LeetCode/Algorithm/DFS_BFS/994.cs
@@ -1,76 +1,62 @@
 ﻿using System.Collections.Generic;
-using System;
 
 namespace _994// 994. 腐烂的橘子
 {// 深度优先DFS 广度优先BFS：
     public class Solution
     {
-        int[] dx = { 1, 0, 0, -1 };
-        int[] dy = { 0, 1, -1, 0 };
+        private int[] dx = { 1, 0, 0, -1 };
+        private int[] dy = { 0, 1, -1, 0 };
 
-        // BFS 广度优先 72ms 39MB
-        public int OrangesRotting_BFS(int[][] grid)
+        // BFS 多源广度优先 80ms 38.9MB
+        public int OrangesRotting(int[][] grid)
         {
-            int row = grid.Length;
-            int column = grid[0].Length;
-            int freshCount = 0, unfreshCount = 0;
-            int min = 0;
-            int tempCount1 = 0, tempCount2 = 0;
+            int row = grid.Length, column = grid[0].Length;
+            int freshCount = 0, min = 0;
             Queue<int[]> queue = new Queue<int[]>();
-            for(int i = 0; i < row; i++)
+
+            for (int i = 0; i < row; i++)
             {
-                for(int j = 0; j < column; j++)
+                for (int j = 0; j < column; j++)
                 {
-                    if(grid[i][j] == 1)
+                    if (grid[i][j] == 1)
                     {
                         freshCount++;
                     }
-                    else if(grid[i][j] == 2)
+                    else if (grid[i][j] == 2)
                     {
                         queue.Enqueue(new int[2] { i, j });
-                        unfreshCount++;
                     }
                 }
             }
             if (freshCount == 0)
-            {
                 return 0;
-            }
-            min++;
-            while(queue.Count > 0)
+
+            while (queue.Count > 0)
             {
-                int[] cell = queue.Dequeue();
-                for(int i = 0; i < 4; i++)
+                int count = queue.Count;
+                min++;
+                for (int k = 0; k < count && freshCount > 0; k++)
                 {
-                    int x = cell[0] + dx[i];
-                    int y = cell[1] + dy[i];
-                    if (x >= 0 && x < row && y >= 0 && y < column)
+                    int[] cell = queue.Dequeue();
+                    for (int i = 0; i < 4 && freshCount > 0; i++)
                     {
-                        if(grid[x][y] == 1)
+                        int x = cell[0] + dx[i];
+                        int y = cell[1] + dy[i];
+                        if (x >= 0 && x < row && y >= 0 && y < column)
                         {
-                            grid[x][y] = 2;
-                            freshCount--;
-                            tempCount2++;
-                            queue.Enqueue(new int[2] { x, y });
-                            if (freshCount == 0)
-                                break;
+                            if (grid[x][y] == 1)
+                            {
+                                grid[x][y] = 2;
+                                queue.Enqueue(new int[2] { x, y });
+                                freshCount--;
+                            }
                         }
                     }
                 }
                 if (freshCount == 0)
-                    break;
-                tempCount1++;
-                if (tempCount1 == unfreshCount)
-                {
-                    min++;
-                    unfreshCount = tempCount2;
-                    tempCount1 = 0;
-                    tempCount2 = 0;
-                }
+                    return min;
             }
-            if (freshCount > 0)
-                return -1;
-            return min;
+            return -1;
         }
     }
 }
--- LeetCode/LeetCode/Program.cs
+++ LeetCode/LeetCode/Program.cs
@@ -7,7 +7,7 @@ internal class Program
     {
         Console.WriteLine("Hello World!");
         Solution sol = new();
-        int min = sol.OrangesRotting_DFS(new int[][] { 
+        int min = sol.OrangesRotting(new int[][] { 
             new int[] { 2, 1, 1}, 
             new int[] { 1, 1, 0}, 
             new int[] { 0, 1, 1}, 
