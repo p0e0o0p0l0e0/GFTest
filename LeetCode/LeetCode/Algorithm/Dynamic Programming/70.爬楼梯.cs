e9f6b5d727278a8154177afa63d48a7dfd604661
CYOU-INC\zhanglinqi <p0e0o0p0l0e0@sina.com>
2022-07-29 16:42:16 +0800

    leetcode: [70] 爬楼梯：斐波那契 通项公式 24ms 26.5MB 时间O(log(n)) 空间O(1)

--- LeetCode/LeetCode/Algorithm/Dynamic Programming/70.爬楼梯.cs
+++ LeetCode/LeetCode/Algorithm/Dynamic Programming/70.爬楼梯.cs
@@ -11,13 +11,48 @@ namespace _70
 
     public class Solution
     {
+        // 24ms 26.5MB 斐波那契数列 通项公式 参考同目录图片 时间O(log(n)) 空间O(1)
+        public int ClimbStairs(int n)
+        {
+            int[][] m = new int[2][] { new int[2] { 0, 1 }, new int[2] { 1, 1 } };
+            int[][] res = pow(m, n);
+            return res[1][1];
+        }
+
+        public int[][] pow(int[][] a, int n)
+        {
+            int[][] ret = new int[2][] { new int[2] { 1, 0 }, new int[2] { 0, 1 } };
+            while(n > 0)
+            {
+                // 二分方式将时间复杂度降为O(log(n))
+                if ((n & 1) == 1) // 如果是奇数就多乘一个a，因为后面n要除以2，向下取整
+                {
+                    ret = multiply(ret, a);
+                }
+                n >>= 1; // 除以2，向下取整
+                a = multiply(a, a);
+            }
+            return ret;
+        }
+
+        // 矩阵乘法
+        public int[][] multiply(int[][] a, int[][] b)
+        {
+            int[][] c = new int[2][]{new int[2], new int[2]};
+            c[0][0] = a[0][0] * b[0][0] + a[0][1] * b[1][0];
+            c[0][1] = a[0][0] * b[0][1] + a[0][1] * b[1][1];
+            c[1][0] = a[1][0] * b[0][0] + a[1][1] * b[1][0];
+            c[1][1] = a[1][0] * b[0][1] + a[1][1] * b[1][1];
+            return c;
+        }
+
         // 24ms 25.9MB DP 时间O(n) 空间O(1) 只需要前两个数据
         public int ClimbStairs_DP2(int n)
         {
             if (n == 1)
                 return 1;
             int first = 1, second = 2;
-            for(int i = 3; i <= n; i++)
+            for (int i = 3; i <= n; i++)
             {
                 int third = first + second;
                 first = second;
@@ -32,13 +67,12 @@ namespace _70
             int[] dp = new int[n + 1];
             dp[0] = 1;
             dp[1] = 1;
-            for(int i = 2; i <= n; i++)
+            for (int i = 2; i <= n; i++)
             {
-                dp[i] = dp[i-1] + dp[i-2];
+                dp[i] = dp[i - 1] + dp[i - 2];
             }
             return dp[n];
         }
-
 
         private Dictionary<int, int> cache = new Dictionary<int, int>();
 
@@ -53,9 +87,9 @@ namespace _70
         private int DP(int n)
         {
             int temp;
-            if(!cache.TryGetValue(n, out temp))
+            if (!cache.TryGetValue(n, out temp))
             {
-                temp = DP(n-1) + DP(n-2);
+                temp = DP(n - 1) + DP(n - 2);
                 cache[n] = temp;
             }
             return temp;
