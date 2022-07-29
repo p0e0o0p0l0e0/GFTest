afeaf05e81fdc1e4f159b39a53bc9625574e55f7
CYOU-INC\zhanglinqi <p0e0o0p0l0e0@sina.com>
2022-07-29 16:19:40 +0800

    leetcode: [70] 爬楼梯：动态规划2 24ms 25.9MB 时间O(n) 空间O(1)

--- LeetCode/LeetCode/Algorithm/Dynamic Programming/70.爬楼梯.cs
+++ LeetCode/LeetCode/Algorithm/Dynamic Programming/70.爬楼梯.cs
@@ -11,9 +11,23 @@ namespace _70
 
     public class Solution
     {
+        // 24ms 25.9MB DP 时间O(n) 空间O(1) 只需要前两个数据
+        public int ClimbStairs_DP2(int n)
+        {
+            if (n == 1)
+                return 1;
+            int first = 1, second = 2;
+            for(int i = 3; i <= n; i++)
+            {
+                int third = first + second;
+                first = second;
+                second = third;
+            }
+            return second;
+        }
 
-        // 20ms 26.5MB
-        public int ClimbStairs(int n)
+        // 24ms 25.9MB DP 时间O(n) 空间O(n)
+        public int ClimbStairs_DP1(int n)
         {
             int[] dp = new int[n + 1];
             dp[0] = 1;
@@ -28,8 +42,8 @@ namespace _70
 
         private Dictionary<int, int> cache = new Dictionary<int, int>();
 
-        // 20ms 26.5MB
-        public int ClimbStairs1(int n)
+        // 20ms 26.5MB 递归 时间O(n) 空间O(n)
+        public int ClimbStairs_Recursion(int n)
         {
             cache[1] = 1;
             cache[2] = 2;
