ec7cf1f288f0b4721171df83766b7901b613ecef
CYOU-INC\zhanglinqi <p0e0o0p0l0e0@sina.com>
2022-08-01 21:02:07 +0800

    leetcode: [191] 位1的个数 位运算

---
+++ LeetCode/LeetCode/Algorithm/Bit Operate/191.位-1-的个数.cs
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode.cn id=191 lang=csharp
+ *
+ * [191] 位1的个数
+ */
+
+namespace _191
+{
+    // @lc code=start
+    public class Solution
+    {
+        // 方法三：二分计数法 20ms 23.2MB O(lognlogn) O(1)
+        /* 
+           0x5555……这个换成二进制之后就是0101010101010101……
+           0x3333……这个换成二进制之后就是0011001100110011……
+           0x0f0f……...这个换成二进制之后就是0000111100001111……
+           对于2位的x，有x中1的个数=(x>>1)+(x&1)。是不是和上面的式子有点像？
+           就是一个 错位分段相加，然后递归合并的过程 
+        */
+        public int HammingWeight(uint n)
+        {
+            n = (n & 0x55555555) + ((n >> 1) & 0x55555555);
+            n = (n & 0x33333333) + ((n >> 2) & 0x33333333);
+            n = (n & 0x0f0f0f0f) + ((n >> 4) & 0x0f0f0f0f);
+            n = (n & 0x00ff00ff) + ((n >> 8) & 0x00ff00ff);
+            n = (n & 0x0000ffff) + ((n >> 16) & 0x0000ffff);
+            return (int)n;
+        }
+
+        // 方法二：位运算优化 20ms 23.1MB O(logn) O(1)
+        // 不断让当前的n与n-1做与运算，直到n变为0即可。
+        // 因为每次运算会使得n的最低位的1被翻转，因此运算次数就等于n的二进制位中1的个数。
+        public int HammingWeight_2(uint n)
+        {
+            int count = 0;
+            while (n != 0)
+            {
+                n &= n - 1;
+                count++;
+            }
+            return count;
+        }
+
+        // 方法一：循环检查二进制位 24ms 23.3MB O(k) O(1) k是int型二进制的位数
+        public int HammingWeight_1(uint n)
+        {
+            int count = 0;
+            for (int i = 0; i < 32; i++)
+            {
+                if (((1 << i) & n) > 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+
+    // @lc code=end
+}
--- LeetCode/LeetCode/Algorithm/Bit Operate/231.2-的幂.cs
+++ LeetCode/LeetCode/Algorithm/Bit Operate/231.2-的幂.cs
@@ -7,12 +7,13 @@
 namespace _231
 {
     // @lc code=start
-    
+
     // 24ms 27.7MB
     public class Solution
     {
         // 方法二：判断是否为最大 22 的幂的约数
-        public bool IsPowerOfTwo(int n) {
+        public bool IsPowerOfTwo(int n)
+        {
             return n > 0 && (1 << 30) % n == 0;
         }
 
@@ -25,14 +26,14 @@ namespace _231
         // 方法一：二进制表示
         public bool IsPowerOfTwo_2(int n)
         {
-            return (n > 0) && (n & (n-1)) == 0;
+            return (n > 0) && (n & (n - 1)) == 0;
         }
-        
+
         public bool IsPowerOfTwo_Mine(int n)
         {
             if (n <= 0)
                 return false;
-            while(n > 1)
+            while (n > 1)
             {
                 int x = n >> 1;
                 if ((x << 1) != n)
